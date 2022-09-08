537aad48deb664006137ad0b8027164a830337c7
Kabourlix <hdamaia@outlook.fr>
2022-09-08 13:41:32 -0400

    Setting up PacMan state system

--- Assets/Scripts/PacMan/PacManStateManager.cs
+++ Assets/Scripts/PacMan/PacManStateManager.cs
@@ -1,19 +1,40 @@
+using System;
 using UnityEngine;
 
 namespace PacMan
 {
     public class PacManStateManager : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        private PacManState _currentState;
+
+        #region States definitions
+
+        public PacManNormalState NormalState { get; private set; }
+        public PacManSuperState SuperState { get; private set; }
+
+        #endregion
+
+        private void Awake()
         {
-        
+            NormalState = new PacManNormalState();
+            SuperState = new PacManSuperState();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Start()
         {
-        
+            ChangeState(NormalState);
+        }
+
+        public void ChangeState(PacManState state)
+        {
+            _currentState?.EndState();
+            _currentState = state;
+            _currentState.EnterState();
+        }
+
+        private void Update()
+        {
+            _currentState.UpdateState();
         }
     }
 }
---
+++ Assets/Scripts/PacMan/PacManSuperState.cs
@@ -0,0 +1,20 @@
+namespace PacMan
+{
+    public class PacManSuperState : PacManState
+    {
+        public override void EnterState()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void UpdateState()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void EndState()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
