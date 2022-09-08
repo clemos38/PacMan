a833fbb10c4965df638a523ec41df4f5fcb2df47
Kabourlix <hdamaia@outlook.fr>
2022-09-08 11:03:54 -0400

    STart creating states for pacman

---
+++ Assets/Scripts/PacMan/PacManStateManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace PacMan
+{
+    public class PacManStateManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
