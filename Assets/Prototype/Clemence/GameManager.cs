da501326af7a2f97d8227a685918b3cf77c3685e
Clémence <clemence.clavel@hotmail.fr>
2022-09-10 19:13:11 +0200

    Tilemap all Pacgums eatable

--- Assets/Prototype/Clemence/GameManager.cs
+++ Assets/Prototype/Clemence/GameManager.cs
@@ -1,102 +1,106 @@
 using Prototype.Clemence;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+namespace CCLH
 {
-    [SerializeField] private Ghost[] ghostsList;
-    public Pacman pacman;
-    public Transform[] pacgums;
-    public int PlayerScore { get; private set; }
-    public int PlayerLives { get; private set; }
-
-    public void Start()
+    public class GameManager : MonoBehaviour
     {
-        NewGame();
-    }
 
-    public void NewGame()
-    {
-        SetScore(0);
-        SetLives(2);
-        ResetGhostsListState();
-        ResetPacmanState();
-    }
+        [SerializeField] private Ghost[] ghostsList;
+        public Pacman pacman;
+        public Transform pacgums;
+        public int PlayerScore { get; private set; }
+        public int PlayerLives { get; private set; }
 
-    public void SetScore(int score)
-    {
-        PlayerScore = score;
-    }
-
-    public void SetLives(int lives)
-    {
-        PlayerLives = lives;
-    }
-
-    public void ResetPacmanState()
-    {
-        pacman.gameObject.SetActive(true);
-    }
-
-    public void ResetGhostsListState()
-    {
-        for (int i = 0; i < ghostsList.Length; i++)
+        public void Start()
         {
-            ghostsList[i].gameObject.SetActive(true);
+            NewGame();
         }
-    }
-    public void ResetPelletsListState()
-    {
 
-    }
-    public void GameOver()
-    {
-        pacman.gameObject.SetActive(false);
-        for (int i = 0; i < ghostsList.Length; i++)
+        public void NewGame()
         {
-            ghostsList[i].gameObject.SetActive(false);
+            SetScore(0);
+            SetLives(2);
+            ResetGhostsListState();
+            ResetPacmanState();
         }
-    }
-    public void PacmanDies()
-    {
-        pacman.gameObject.SetActive(false);
-        SetLives(PlayerLives - 1);
-        if (PlayerLives == 0)
+
+        public void SetScore(int score)
         {
-            GameOver();
+            PlayerScore = score;
         }
-    }
 
-    public void EatGhost(Ghost ghost)
-    {
-        SetScore(PlayerScore + ghost.points);
-    }
-
-    public void EatPacgum(Pacgum pacgum)
-    {
-        pacgum.gameObject.SetActive(false);
-        SetScore(PlayerScore + pacgum.points);
-        if (!HasRemainingPacgums())
+        public void SetLives(int lives)
         {
-            //lancer l'UI pour rejouer ou quitter
-            Debug.Log("you win");
+            PlayerLives = lives;
         }
-    }
 
-    public void EatSuperPacgum(SuperPacgum superPacgum)
-    {
-        EatPacgum(superPacgum);
-        //changer etat fantomes
-    }
-
-    public bool HasRemainingPacgums()
-    {
-        foreach(Transform pacgum in pacgums)
+        public void ResetPacmanState()
         {
-            if (pacgum.gameObject.activeSelf)
+            pacman.gameObject.SetActive(true);
+        }
+
+        public void ResetGhostsListState()
+        {
+            for (int i = 0; i < ghostsList.Length; i++)
             {
-                return true;
+                ghostsList[i].gameObject.SetActive(true);
             }
         }
-        return false;
+        public void ResetPelletsListState()
+        {
+
+        }
+        public void GameOver()
+        {
+            pacman.gameObject.SetActive(false);
+            for (int i = 0; i < ghostsList.Length; i++)
+            {
+                ghostsList[i].gameObject.SetActive(false);
+            }
+        }
+        public void PacmanDies()
+        {
+            pacman.gameObject.SetActive(false);
+            SetLives(PlayerLives - 1);
+            if (PlayerLives == 0)
+            {
+                GameOver();
+            }
+        }
+
+        public void EatGhost(Ghost ghost)
+        {
+            SetScore(PlayerScore + ghost.points);
+        }
+
+        public void EatPacgum(Pacgum pacgum)
+        {
+            pacgum.gameObject.SetActive(false);
+            SetScore(PlayerScore + pacgum.points);
+            if (!HasRemainingPacgums())
+            {
+                //lancer l'UI pour rejouer ou quitter
+                Debug.Log("you win");
+            }
+        }
+
+        public void EatSuperPacgum(SuperPacgum superPacgum)
+        {
+            EatPacgum(superPacgum);
+            //changer etat fantomes
+        }
+
+        public bool HasRemainingPacgums()
+        {
+            foreach (Transform pacgum in pacgums)
+            {
+                if (pacgum.gameObject.activeSelf)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
--- Assets/Prototype/Clemence/Pacgum.cs
+++ Assets/Prototype/Clemence/Pacgum.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pacgum : MonoBehaviour
+namespace CCLH
 {
-    public int points = 10;
+    public class Pacgum : MonoBehaviour
+    {
+        public int points = 10;
 
-    public void OnTriggerEnter2D(Collider2D collider)
-    {
-        if (collider.gameObject.layer == LayerMask.NameToLayer("Pacman"))
+        public void OnTriggerEnter2D(Collider2D collider)
         {
-            GetEaten();
+            if (collider.gameObject.layer == LayerMask.NameToLayer("Pacman"))
+            {
+                GetEaten();
+            }
         }
-    }
-    protected virtual void GetEaten()
-    {
-        FindObjectOfType<GameManager>().EatPacgum(this);
+        protected virtual void GetEaten()
+        {
+            FindObjectOfType<GameManager>().EatPacgum(this);
+        }
     }
 }
